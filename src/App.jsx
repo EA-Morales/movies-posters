5acc0caaa5b3429a9995f7375c14bd470599bb36
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-06-25 00:46:19 -0300

    refactor reducer and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
-import { searchTrendingMovies } from './lib/api/search-trending-movies';
+import { useMoviesSearch } from './lib/hooks/useMoviesSearch';
 import './styles/App.css';
 
 function App() {
-	const { movies, page, loading, error, setpage } = useMovies();
+	const { movies, page, loading, error, setPage } = useMoviesSearch();
 
 	if (loading) return <div>Loading...</div>;
 
@@ -12,37 +11,12 @@ function App() {
 	return (
 		<div className='container mx-auto'>
 			<div className='flex gap-4'>
-				<button onClick={() => setpage(page + -1)}>{page} page</button>
-				<button onClick={() => setpage(page + 1)}>page {page}</button>
+				<button onClick={() => setPage(page + -1)}>{page} page</button>
+				<button onClick={() => setPage(page + 1)}>page {page}</button>
 			</div>
 			{movies && movies.map(movie => <p key={movie.id}>{movie.title}</p>)}
 		</div>
 	);
 }
-
-const searchTrending = async (page, setmovies, setError, setLoading) => {
-	setLoading(true);
-	const { success, data, statusCode } = await searchTrendingMovies(page);
-
-	if (success) setmovies(data.movies);
-	else {
-		setmovies();
-		setError('Error: ' + statusCode);
-	}
-	setLoading(false);
-};
-
-const useMovies = () => {
-	const [movies, setmovies] = useState([]);
-	const [page, setpage] = useState(1);
-	const [error, setError] = useState();
-	const [loading, setLoading] = useState(false);
-
-	useEffect(() => {
-		searchTrending(page, setmovies, setError, setLoading);
-	}, [page]);
-
-	return { movies, page, loading, error, setpage };
-};
 
 export default App;
