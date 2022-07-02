69fa8062a5e6a19694b55179eda23a78fb7bc030
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-07-01 21:13:48 -0300

    minor changes
    some changes in styles rating, year, etc

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import CrossIcon from './icons/CrossIcon';
 
 const Modal = ({ children, closeModal }) => {
+	useEffect(() => {
+		document.body.classList.add('overflow-y-hidden');
+
+		return () => document.body.classList.remove('overflow-y-hidden');
+	}, []);
 	return createPortal(
 		<div className='flex justify-center items-center fixed inset-0 p-24 bg-slate-800/60'>
 			<div className='relative w-3/4 mx-auto p-4 bg-black'>
--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -23,10 +23,12 @@ const MovieCard = ({
 				style={{ backgroundImage: `url(${image})` }}
 				className='aspect-2/3 bg-cover p-2 relative bg-slate-500 hover:scale-110'>
 				<div className='flex justify-between'>
-					<span className='bg-netflix-primary rounded-full p-1 text-sm'>
+					<span className='bg-netflix-primary p-1 text-sm absolute top-0 left-0'>
 						{rating}
 					</span>
-					<span className='bg-netflix-primary py-1 px-2 text-sm'>{year}</span>
+					<span className='bg-netflix-background py-1 px-2 text-sm absolute top-0 right-0'>
+						{year}
+					</span>
 				</div>
 			</div>
 		</div>
